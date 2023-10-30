4193139c500023fb4286ce1b65b80d0c89b77979
axelfoxii <asfoxii@gmail.com>
2023-10-29 23:34:46 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   @ViewChild('videoContainer', {static: true}) videoContainer!: ElementRef;
  
   
- 
+  /* Este error se debe a que angular-cli no admite módulos en nodos como "fs" y "path". Agregue lo siguiente al archivo "package.json" */
 
   
   ngOnInit() {
@@ -27,25 +27,26 @@ export class AppComponent implements OnInit {
     var video = await navigator.mediaDevices.getUserMedia({ video: true });  
 
     await faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models');
-    await faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models');
-    await faceapi.nets.faceRecognitionNet.loadFromUri('/assets/models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models');    
     await faceapi.nets.faceExpressionNet.loadFromUri('/assets/models');
-   
+    await faceapi.nets.ageGenderNet.loadFromUri('/assets/models');
     let stream =  this.videoContainer.nativeElement
     stream.srcObject = video;
 
   }  
 
+  /* @types/webgl2 WebGL (Biblioteca de gráficos web) y una API de JavaScript para renderizar gráficos 3D y 2D interactivos de alto rendimiento dentro de cualquier navegador web compatible sin el uso de complementos.Y se puede usar con la etiqueta HTML5 <canvas>
+ */
 
-  playEvento(){
-
-    
+  playEvento(){    
     
     this.videoContainer.nativeElement.addEventListener('play',()=>{
 
       const canvas = faceapi.createCanvasFromMedia(this.videoContainer.nativeElement); 
 
     document.body.append(canvas);
+
+    canvas.setAttribute('id','idcanvas');
 
      const displaySize = {width:this.videoContainer.nativeElement.width, height:this.videoContainer.nativeElement.height};
 
@@ -56,6 +57,7 @@ export class AppComponent implements OnInit {
         const detections = await faceapi.detectAllFaces(this.videoContainer.nativeElement, new faceapi.TinyFaceDetectorOptions())
         .withFaceLandmarks()
         .withFaceExpressions()
+        .withAgeAndGender()
 
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
@@ -64,13 +66,18 @@ export class AppComponent implements OnInit {
         faceapi.draw.drawDetections(canvas,resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas,resizedDetections);
         faceapi.draw.drawFaceExpressions(canvas,resizedDetections);
+        resizedDetections.forEach( detection => {
+          const box = detection.detection.box
+          const drawBox = new faceapi.draw.DrawBox(box, { label: Math.round(detection.age) + " year old " + detection.gender })
+          drawBox.draw(canvas)
+        }) 
 
-
-      },1000)
+      },1000);
 
 
     })
 
   }
 
+ 
 }
